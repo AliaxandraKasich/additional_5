e0e63964c927003792e57b7244826d9973cd1619
AliaxandraKasich <Sane4ka022@mail.ru>
2018-02-21 23:48:45 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   var arr = [];
+   var stack = [];
+   var bool = true;
+   for(var i=0; i < str.length ; i++){
+      arr.push(str.charAt(i));
+   }
+
+   for(var i=0; i< arr.length; i++){
+     if(arr[i] === ")")  arr[i] = 9;
+     if(arr[i] === "(")  arr[i] = 1;
+     if(arr[i] === "[")  arr[i] = 3;
+     if(arr[i] === "]")  arr[i] = 7;
+     if(arr[i] === "{")  arr[i] = 2;
+     if(arr[i] === "}")  arr[i] = 8;
+     if(arr[i] === "|")  arr[i] = 5;
+   }
+   var arrBool = [];
+   for(var i=0; i< arr.length;i++){
+     arrBool[i] = false;
+   }
+
+   for(var i=0; i< arr.length;i++){
+     if(arrBool[i] === false){
+      if(stack.length === 0){
+        stack.push(arr[i]);
+      }
+      else{
+      var temp = stack.pop();
+      stack.push(temp);
+         if(temp + arr[i] === 10 ){
+          if(temp <= arr[i]){
+            stack.pop();
+           }
+        else{
+          stack.push(arr[i]);
+         }
+        }
+        else{ stack.push(arr[i]);}
+      }
+     }
+  }
+  if(stack.length !=0){
+    bool = false;
+  }
+  return bool;
 }
